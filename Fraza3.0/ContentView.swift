11e084a74e05d64fc7095a60023f562bb29e697c
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-10 10:56:49 +0300

    Initial Commit

---
+++ Fraza3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fraza3.0
+//
+//  Created by 上野邦彦 on 2020/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

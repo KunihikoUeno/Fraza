5e650fbc68bc09bca73bb9ee4209f858a7cf8945
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-11 21:25:21 +0300

    create CustomNavigationView

---
+++ Fraza3.0/Common/Components/CustomNavigationView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct CustomNavigationView<Content>: View where Content: View {
+    
+    let title: String
+    let content: () -> Content
+    
+    init(title: String, @ViewBuilder content: @escaping () -> Content) {
+        self.title = title
+        self.content = content
+        setupNavigatioinBarStyle()
+    }
+    
+    private func setupNavigatioinBarStyle() {
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().barStyle = UIBarStyle.black
+        UINavigationBar.appearance().barTintColor = UIColor.customBlue
+        if let font = UIFont(name: "HelveticaNeue-Light", size: 18) {
+            UINavigationBar.appearance().titleTextAttributes = [.font : font]
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            content()
+                .navigationBarTitle(title, displayMode: .inline)
+        }
+    }
+    
+}
+
+struct CustomNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomNavigationView(title: "Title") {
+            Text("Some Content")
+        }
+    }
+}

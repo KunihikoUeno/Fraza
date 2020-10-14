0a1a53519f2a4aa8c57c80f4dd46f03e8a37bdf3
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 00:32:21 +0300

    set navigartion view and text to favorite view

--- Fraza3.0/Root/Favorite/FavoriteView.swift
+++ Fraza3.0/Root/Favorite/FavoriteView.swift
@@ -2,7 +2,13 @@ import SwiftUI
 
 struct FavoriteView: View {
     var body: some View {
-        Text("Favorite")
+        CustomNavigationView(title: "Favorite") {
+            Text("Tap the heart icon\nunder the play button\nto make your favorite list.")
+                .font(.custom("HelveticaNeue-Light", size: 17))
+                .foregroundColor(.textColor)
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+        }
     }
 }
 

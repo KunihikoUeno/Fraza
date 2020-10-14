a07b52838aca166b96428aa27087c1f82aa6b611
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-14 23:26:45 +0300

    create SearchBarView

---
+++ Fraza3.0/Root/Search/SeachBar/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  Fraza3.0
+//
+//  Created by 上野邦彦 on 2020/10/14.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}

8234a6590ebbdb64d3c553fb9ee19e08d58d82b0
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 02:32:53 +0300

    open contact view from info list view

--- Fraza3.0/Root/Info/InfoList/InfoListView.swift
+++ Fraza3.0/Root/Info/InfoList/InfoListView.swift
@@ -8,17 +8,6 @@ struct InfoListView: View {
         case contact = "Contact us"
     }
     
-    private func tapDetected(_ item: InfoListItem) {
-        switch item {
-        case .rate:
-            rateAction()
-        case .feedback:
-            print("feedback tapped")
-        case .contact:
-            print("contact tapped")
-        }
-    }
-    
     private func rateAction() {
         guard let url = URL(string : "itms-apps://itunes.apple.com/app/id1319113132") else { return }
         UIApplication.shared.open(url, options: [:])
@@ -27,11 +16,18 @@ struct InfoListView: View {
     var body: some View {
         VStack(spacing: 0) {
             ForEach(InfoListItem.allCases, id: \.self) { item in
-                InfoListItemView(title: "\(item.rawValue)")
-                    .onTapGesture { tapDetected(item) }
+                if case InfoListItem.rate = item {
+                    InfoListItemView(title: "\(item.rawValue)")
+                        .onTapGesture { rateAction() }
+                } else {
+                    NavigationLink(destination: ContactView()) {
+                        InfoListItemView(title: "\(item.rawValue)")
+                    }
+                }
             }
         }
     }
+    
 }
 
 struct InfoListView_Previews: PreviewProvider {

4c6f161675979b5a98636f2665e5908f9e0d69af
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-14 22:21:33 +0300

    apply PhraseListView to category view

--- Fraza3.0/Root/Home/Category/CategoryView.swift
+++ Fraza3.0/Root/Home/Category/CategoryView.swift
@@ -4,7 +4,6 @@ struct CategoryView: View {
     
     private let categoryId: Int
     private let title: String
-    @State var activeItem: Int?
     
     init(categoryId: Int, title: String) {
         self.categoryId = categoryId
@@ -12,25 +11,9 @@ struct CategoryView: View {
         setupPhraseData()
     }
     
-    private func tapDetected(_ index: Int) {
-        activeItem = index
-    }
-    
     var body: some View {
         CustomNavigationView(title: title) {
-            ScrollView {
-                VStack(spacing: 0) {
-                    ForEach(0..<phraseData[categoryId].count) { index in
-                        let phrase = phraseData[categoryId][index]
-                        if let activeItem = activeItem, activeItem == index {
-                            PhraseSelectedView(phrase)
-                        } else {
-                            PhraseDefaultView(phrase)
-                                .onTapGesture { tapDetected(index) }
-                        }
-                    }
-                }
-            }
+            PhraseListView(phraseData, categoryId: categoryId)
         }
     }
     

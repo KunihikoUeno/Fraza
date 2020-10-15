04fb27871f065ae08b2d3d43f7d1e59aabf4089e
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 16:28:46 +0300

    phrase models created

---
+++ Fraza3.0/Common/Model/Data/Phrase/Phrase.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+//struct Phrase: Decodable {
+//    
+//    let id: Int
+//    let categoryId: Int
+//    let title: String
+//    let phrase: String
+//    let pronunciation: String
+//    let audio: String
+//    
+//    init(
+//        id: Int,
+//        categoryId: Int,
+//        title: String,
+//        phrase: String,
+//        pronunciation: String,
+//        audio: String
+//    ) {
+//        self.id = id
+//        self.categoryId = categoryId
+//        self.title = title
+//        self.phrase = phrase
+//        self.pronunciation = pronunciation
+//        self.audio = audio
+//    }
+//    
+//}

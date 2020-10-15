f1f49af7d53fcf94ebb0ee86a63321d1ac92a533
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 20:48:49 +0300

    feat: favorite list created

--- Fraza3.0/Root/Favorite/FavoriteView.swift
+++ Fraza3.0/Root/Favorite/FavoriteView.swift
@@ -1,15 +1,30 @@
 import SwiftUI
 
 struct FavoriteView: View {
+    
+    var phrases: [Phrase]?
+    @ObservedObject var bookmarks = Bookmarks()
+    
+    init() {
+        self.phrases = DataManager
+            .fetchData(type: Phrase.self, fileName: "Phrases")
+            .filter { bookmarks.mylist.contains($0.id) }
+    }
+    
     var body: some View {
         CustomNavigationView(title: "Favorite") {
-            Text("Tap the heart icon\nunder the play button\nto make your favorite list.")
-                .font(.custom("HelveticaNeue", size: 17))
-                .foregroundColor(.textColor)
-                .multilineTextAlignment(.center)
-                .lineSpacing(8)
+            if let phrases = phrases {
+                PhraseListView(phrases)
+            } else {
+                Text("Tap the heart icon\nunder the play button\nto make your favorite list.")
+                    .font(.custom("HelveticaNeue", size: 17))
+                    .foregroundColor(.textColor)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(8)
+            }
         }
     }
+    
 }
 
 struct FavoriteView_Previews: PreviewProvider {

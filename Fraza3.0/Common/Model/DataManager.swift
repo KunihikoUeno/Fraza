002b175da28d4473746334cdb230e17df2ec6481
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 16:37:09 +0300

    refactor: change CategoryManage to DataManager

---
+++ Fraza3.0/Common/Model/DataManager.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    func fetchData<T: Decodable>(type: T.Type, fileName: String) -> [T] {
+        do {
+            guard let filepath = Bundle.main.url(forResource: fileName, withExtension: "plist") else { return [] }
+            let data = try Data(contentsOf: filepath)
+            return try PropertyListDecoder().decode([T].self, from: data)
+        } catch {
+            fatalError("Error occured with Categories.plist: \(error.localizedDescription)")
+        }
+    }
+    
+}

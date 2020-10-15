7ec1d4deed6a184e04a0a88761af45af344bcdb3
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-15 18:09:04 +0300

    refactor: make DataManager non singleton

--- Fraza3.0/Root/Search/SearchView.swift
+++ Fraza3.0/Root/Search/SearchView.swift
@@ -5,7 +5,7 @@ struct SearchView: View {
     @State private var searchWord = ""
     
     var body: some View {
-        let phrases = DataManager.shared.fetchData(type: Phrase.self, fileName: "Phrases")
+        let phrases = DataManager.fetchData(type: Phrase.self, fileName: "Phrases")
         let filteredPhrases = searchWord.count > 0
             ? phrases.filter {
                 $0.title.lowercased().contains(searchWord.lowercased())

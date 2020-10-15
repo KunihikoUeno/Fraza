c953fcef02c16053901bf5522c9960519bbce671
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-16 00:32:37 +0300

    feat: set custom back button in navigation bar

--- Fraza3.0/Root/Info/Contact/ContactView.swift
+++ Fraza3.0/Root/Info/Contact/ContactView.swift
@@ -4,6 +4,7 @@ import MessageUI
 struct ContactView: View {
     
     @State private var isPresented = false
+    @Environment(\.presentationMode) var presentationMode
     
     private func sendAction() {
         print("send button tapped")
@@ -11,26 +12,32 @@ struct ContactView: View {
     }
     
     var body: some View {
-        CustomNavigationView(title: "Contact us") {
-            VStack(spacing: 0) {
-                Text("We love to hear your opinions on our app!\nPlease send us your thoughts.")
-                    .font(.custom("HelveticaNeue", size: 16))
-                    .foregroundColor(.textColor)
-                    .multilineTextAlignment(.center)
-                    .lineSpacing(6)
-                    .padding(.bottom, 15)
-                Button(action: {
-                    self.isPresented.toggle()
-                }) {
-                    Image("send")
-                }
-                .disabled(!MFMailComposeViewController.canSendMail())
-                .sheet(isPresented: $isPresented, content: {
-                    MailView(isShowing: self.$isPresented)
-                })
-                .padding(.bottom, 5)
+        VStack(spacing: 0) {
+            Text("We love to hear your opinions on our app!\nPlease send us your thoughts.")
+                .font(.custom("HelveticaNeue", size: 16))
+                .foregroundColor(.textColor)
+                .multilineTextAlignment(.center)
+                .lineSpacing(6)
+                .padding(.bottom, 15)
+            Button(action: {
+                self.isPresented.toggle()
+            }) {
+                Image("send")
             }
+            .disabled(!MFMailComposeViewController.canSendMail())
+            .sheet(isPresented: $isPresented, content: {
+                MailView(isShowing: self.$isPresented)
+            })
+            .padding(.bottom, 5)
         }
+        .navigationBarTitle("Contact us", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("< Back")
+                .font(.custom("HelveticaNeue-Light", size: 16))
+        })
     }
     
 }

266311172993c02c5be0d42a686474e5bd232bec
Kunihiko Ueno <uenokunihiko@gmail.com>
2020-10-17 15:15:43 +0300

    refactor: adopt LazyVGrid to replace with old CollectionView like implementation
    - decide to fully unsupport iOS 13

--- Fraza3.0/Root/Home/HomeView.swift
+++ Fraza3.0/Root/Home/HomeView.swift
@@ -2,50 +2,34 @@ import SwiftUI
 
 struct HomeView: View {
     
-    private var categories: [Category] = DataManager.fetchData(type: Category.self, fileName: "Categories")
-    private let numberOfColumns = 3
-    
-    private var numberOfRows: Int {
-        return categories.count % 3 > 0
-            ? categories.count / 3 + 1
-            : categories.count / 3
-    }
-    
-    private func getIndex<T>(for items: [T], _ row: Int, _ column: Int, _ numberOfColumns: Int) -> Int? {
-        let index = row * numberOfColumns + column
-        return index < items.count ? index : nil
-    }
+    let categories = DataManager.fetchData(type: Category.self, fileName: "Categories")
+    let numberOfColumns = 3
     
     var body: some View {
         CustomNavigationView(title: "Home") {
             GeometryReader { geometry in
+                
+                let columns: [GridItem] = Array(repeating: .init(.flexible()), count: numberOfColumns)
+                let gridLength = geometry.size.width / CGFloat(numberOfColumns)
+                
                 ScrollView {
-                    VStack(spacing: 0) {
-                        ForEach(0..<numberOfRows) { row in
-                            HStack(spacing: 0) {
-                                ForEach(0..<numberOfColumns) { column in
-                                    if let index = getIndex(for: categories, row, column, numberOfColumns) {
-                                        VStack(spacing: 0) {
-                                            NavigationLink(destination: CategoryView(categoryId: index, title: categories[index].title)) {
-                                                VStack(spacing: 0) {
-                                                    Image(categories[index].image)
-                                                        .padding(.bottom, 5)
-                                                    Text("\(categories[index].title)")
-                                                        .font(.custom("HelveticaNeue", size: 14))
-                                                        .foregroundColor(.textColor)
-                                                }
-                                                .padding(.top, 21)
-                                            }
-                                        }
-                                        .frame(width: geometry.size.width / CGFloat(numberOfColumns), height: geometry.size.width / CGFloat(numberOfColumns))
-                                    } else {
-                                        Spacer()
-                                    }
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        ForEach(0..<categories.count) { index in
+                            NavigationLink(destination: CategoryView(categoryId: index, title: categories[index].title)) {
+                                VStack(spacing: 0) {
+                                    Image(categories[index].image)
+                                        .padding(.bottom, 5)
+                                    Text("\(categories[index].title)")
+                                        .font(.custom("HelveticaNeue", size: 14))
+                                        .foregroundColor(.textColor)
                                 }
+                                .padding(.top, 21)
                             }
                         }
+                        .frame(width: gridLength, height: gridLength)
                     }
                 }
+                
             }
         }
     }
